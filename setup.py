ee73aea8c51ae6bacb569e87309d9d56164f0289
Omer Mohamed Ali <envomer@users.noreply.github.com>
2022-01-22 17:06:34 +0100

    Add setup and requirements.txt

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='osiris',
+    version='',
+    packages=['osiris', 'postmortems', 'postmortems.migrations'],
+    url='',
+    license='',
+    author='omer',
+    author_email='',
+    description='',
+    install_requires=['django', 'django-environ', 'mysqlclient']
+)
