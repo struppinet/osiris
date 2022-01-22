fd07e2b11212cd54bec05ff610560df72ef2b947
Omer Mohamed Ali <envomer@users.noreply.github.com>
2022-01-22 17:06:39 +0100

    Changes by struppi

--- postmortems/admin.py
+++ postmortems/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import PostMortem, PostMortemLog
 
 
-admin.site.register(PostMortem)
+@admin.register(PostMortem)
+class PostMortemAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    pass
+
+
 admin.site.register(PostMortemLog)
