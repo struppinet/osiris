b84bdd98485a097dd30595ea90fedd51169a1d34
Omer Mohamed Ali <envomer@users.noreply.github.com>
2022-01-22 16:42:52 +0100

    Changes by struppi

--- postmortems/admin.py
+++ postmortems/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import PostMortem, PostMortemLog
 
-# Register your models here.
+
+admin.site.register(PostMortem)
+admin.site.register(PostMortemLog)
--- postmortems/models.py
+++ postmortems/models.py
@@ -1,4 +1,19 @@
 from django.db import models
 import datetime
 
-# Create your models here.
+
+class PostMortem(models.Model):
+    name = models.CharField(max_length=255)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class PostMortemLog(models.Model):
+    message = models.TextField(default=None)
+    author = models.CharField(max_length=255, null=True, default=None)
+    datetime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.datetime.strftime('%Y-%m-%d %H:%M:%S') + " - " + self.message
